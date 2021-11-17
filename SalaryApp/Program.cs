39cb7dc77674f9155595aa9f59ff3a7675f263d9
Dennis Lindquist <dennislindquist@gmail.com>
2021-11-17 18:03:17 +0100

    Add project files.

---
+++ SalaryApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SalaryApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public void Test()
+        {
+
+        }
+    }
+}
---
+++ SalaryAppTests/ProgramTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SalaryApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryApp.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void TestTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
+
+namespace SalaryAppTests
+{
+    internal class ProgramTests
+    {
+    }
+}
