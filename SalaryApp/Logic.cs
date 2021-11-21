2efca8fb046a6cacefc37262ac9d2583a0abaa75
Dennis Lindquist <dennislindquist@gmail.com>
2021-11-21 04:15:03 +0100

    testing time now

--- SalaryApp/Logic.cs
+++ SalaryApp/Logic.cs
@@ -31,28 +31,40 @@ namespace SalaryApp
 
             while (run)
             {
+                Console.WriteLine("-----------------------------------------------");
+                Console.WriteLine("Login Menu");
+                Console.WriteLine("-----------------------------------------------");
                 Console.Write("Username: ");
                 string username = Console.ReadLine();
                 Console.Write("Password: ");
                 string password = Console.ReadLine();
+                if (!StringIsNullEmptyOrWhiteSpace(username) && !StringIsNullEmptyOrWhiteSpace(password))
+                {
+                    var userAccount = Login(username, password);
 
-                var userAccount = Login(username, password);
-
-                if (userAccount == null)
-                {
-                    Console.WriteLine("Incorrect Username or Password.");
-                }
-                else if (userAccount.IsAdmin)
-                {
-                    AdminMenu(userAccount);
-                }
-                else if (!userAccount.IsAdmin)
-                {
-                    UserMenu(userAccount);
+                    if (userAccount != null)
+                    {
+                        if (userAccount.IsAdmin)
+                        {
+                            AdminMenu(userAccount);
+                        }
+                        else if (!userAccount.IsAdmin)
+                        {
+                            UserMenu(userAccount);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Something went wrong.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Incorrect Username and/or Password.");
+                    }
                 }
                 else
                 {
-                    Console.WriteLine("Something went wrong.");
+                    Console.WriteLine("Wrong input, try again.");
                 }
             }
         }
@@ -61,11 +73,16 @@ namespace SalaryApp
         {
             bool run = true;
 
+            Console.Clear();
+
             while (run)
             {
+                Console.WriteLine("-----------------------------------------------");
+                Console.WriteLine($"User Menu | User: {userAccount.Name}");
+                Console.WriteLine("-----------------------------------------------");
                 Console.WriteLine("1. See your salary");
                 Console.WriteLine("2. See your role");
-                Console.WriteLine("3. Remove your user");
+                Console.WriteLine("3. Remove your account");
                 Console.WriteLine("0. Logout");
                 if (int.TryParse(Console.ReadLine(), out input))
                 {
@@ -78,18 +95,29 @@ namespace SalaryApp
                             Console.WriteLine($"Role: {userAccount.Role}");
                             break;
                         case 3:
+                            Console.WriteLine("Remove your account");
                             Console.Write("Username: ");
                             string username = Console.ReadLine();
                             Console.Write("Password: ");
                             string password = Console.ReadLine();
 
-                            if (username == userAccount.Name && password == userAccount.Password)
+                            if (!StringIsNullEmptyOrWhiteSpace(username) && !StringIsNullEmptyOrWhiteSpace(password))
                             {
-                                accounts.Remove(userAccount);
+                                if (username == userAccount.Name && password == userAccount.Password)
+                                {
+                                    accounts.Remove(userAccount);
+                                    Console.WriteLine("Your user have been successfully removed.");
+                                    Console.WriteLine("You have been logged out.");
+                                    run = false;
+                                }
+                                else
+                                {
+                                    Console.WriteLine("Incorrect Username and/or Password.");
+                                }
                             }
                             else
                             {
-                                Console.WriteLine("Incorrect username and/or password.");
+                                Console.WriteLine("Wrong input.");
                             }
                             break;
                         case 0:
@@ -99,7 +127,6 @@ namespace SalaryApp
                             Console.WriteLine("Wrong input, try again.");
                             break;
                     }
-
                 }
                 else
                 {
@@ -111,27 +138,55 @@ namespace SalaryApp
         private void AdminMenu(Account userAccount)
         {
             bool run = true;
+            string username;
+            string password;
+
+            Console.Clear();
+
             while (run)
             {
-                Console.WriteLine("1. See salary");
-                Console.WriteLine("2. See role");
-                Console.WriteLine("3. See all users");
-                Console.WriteLine("4. Create user");
-                Console.WriteLine("5. Remove user");
+                Console.WriteLine("-----------------------------------------------");
+                Console.WriteLine($"Admin Menu | Admin: {userAccount.Name}");
+                Console.WriteLine("-----------------------------------------------");
+                Console.WriteLine("1. See your salary");
+                Console.WriteLine("2. See your role");
+                Console.WriteLine("3. See all accounts");
+                Console.WriteLine("4. Create account");
+                Console.WriteLine("5. Remove account");
                 Console.WriteLine("0. Logout");
                 if (int.TryParse(Console.ReadLine(), out input))
                 {
                     switch (input)
                     {
                         case 1:
+                            Console.WriteLine($"Salary: {userAccount.Salary}");
                             break;
                         case 2:
+                            Console.WriteLine($"Role: {userAccount.Role}");
                             break;
                         case 3:
+                            foreach (var account in accounts)
+                            {
+                                Console.WriteLine($"Name: { account.Name} | Password: {account.Password}");
+                            }
                             break;
                         case 4:
+                            Console.WriteLine("Create a new account");
+                            Console.Write("Username: ");
+                            username = Console.ReadLine();
+                            Console.Write("Password: ");
+                            password = Console.ReadLine();
+
+                            CreateNewAccount(username, password);
                             break;
                         case 5:
+                            Console.WriteLine("Remove a account");
+                            Console.Write("Username: ");
+                            username = Console.ReadLine();
+                            Console.Write("Password: ");
+                            password = Console.ReadLine();
+
+                            RemoveAccount(username, password);
                             break;
                         case 0:
                             run = false;
@@ -148,6 +203,57 @@ namespace SalaryApp
             }
         }
 
+        private void RemoveAccount(string username, string password)
+        {
+            if (!StringIsNullEmptyOrWhiteSpace(username) && !StringIsNullEmptyOrWhiteSpace(password))
+            {
+                foreach (var account in accounts)
+                {
+                    if (account.Name == username && account.Password == password)
+                    {
+                        accounts.Remove(account);
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong input.");
+            }
+        }
+
+        private void CreateNewAccount(string username, string password)
+        {
+            if (!StringIsNullEmptyOrWhiteSpace(username) && !StringIsNullEmptyOrWhiteSpace(password))
+            {
+                Console.WriteLine("Is the new user Admin, y/n?");
+                string adminOrNot = Console.ReadLine().ToLower();
+                if (!StringIsNullEmptyOrWhiteSpace(adminOrNot))
+                {
+                    switch (adminOrNot)
+                    {
+                        case "y":
+                            accounts.Add(new Admin { Name = username, Password = password });
+                            break;
+                        case "n":
+                            accounts.Add(new User { Name = username, Password = password });
+                            break;
+                        default:
+                            Console.WriteLine("Something went wrong.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Wrong input.");
+            }
+        }
+
         public Account Login(string name, string password)
         {
             foreach (var account in accounts)
@@ -155,6 +261,11 @@ namespace SalaryApp
                 if (account != null && account.Name == name && account.Password == password) { return account; }
             }
             return null;
+        }
+
+        public bool StringIsNullEmptyOrWhiteSpace(string str)
+        {
+            return string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
         }
     }
 }
